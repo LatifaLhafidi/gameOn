4fd68fa60326e3cd444cfefcee659b878f0a0395
LatifaLhafidi <lhafidilatifa59@gmail.com>
2023-11-18 19:51:13 +0100

    ajouter formFirstIsValid

--- modal.js
+++ modal.js
@@ -1,107 +1,188 @@
 
+// DOM Elements
 const modalBackground = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const modalClose = document.querySelector(".close");
 const formData = document.querySelectorAll(".formData");
-const modalclose = document.querySelector(".close");
-const firstInput = document.querySelector("#first");
-const lastInput = document.querySelector("#last");
-const emailInput = document.querySelector("#email");
-const birthdateInput = document.querySelector("#birthdate");
-const quantityInput = document.querySelector("#quantity");
-const location1 = document.querySelector("#location1");
-const conditionsCheckbox = document.querySelector('#checkbox1');
-const form = document.querySelector("form");
 
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-modalclose.addEventListener("click", closeModal);
+const modalBody = document.querySelector(".modal-body");
+const modalSuccess = document.querySelector(".modal-success")
+const modalCloseSucess = document.querySelector('.modal-success-close');
+
+const formFirst = document.getElementById('first'); // Get firstname input
+const formLast = document.getElementById('last'); // Get lastname input
+const formEmail = document.getElementById('email'); // Get email input
+const formBirthdate = document.getElementById('birthdate'); // Get birthdate input
+const formQuantity = document.getElementById('quantity'); // Get quantity of number of tournament participated input
+const formLocation = document.querySelector('input[name="location"]'); // Get location input radio
+let formLocationCheck = document.querySelector('input[name="location"]:checked'); // Get location of next tournament input radio check
+const formTermsConditions = document.getElementById('checkbox1'); // Get terms conditions input checkbox
+
 //RegEx
-const regexName = /^[a-zA-ZÀ-ÖØ-öø-ÿ]{2,}$/;
-const regexEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const regexName = /^[a-z-A-Z ,.'-]+$/;
 const regexQuantity = /^([0-9]{1,2})$/;
-const dateNaissanceRegex = /^(19|20)\d\d-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/;
-
-
-// Check input with event listen
-firstInput.addEventListener("input", () => CheckInputValue(regexName, firstInput));
-lastInput.addEventListener("input", () => CheckInputValue(regexName, lastInput));
-emailInput.addEventListener("input", () => CheckInputValue(regexEmail, emailInput));
-birthdateInput.addEventListener("input", () => CheckInputValue(dateNaissanceRegex, birthdateInput));
-quantityInput.addEventListener("input", () => CheckInputValue(regexQuantity, quantityInput));
-// location1.addEventListener("invalid", ErrorMessage);
-conditionsCheckbox.addEventListener("input", checkIfConditionsAccepted(conditionsCheckbox));
-// form.addEventListener("submit", handleSubmit);
-
-//checkIfConditionsAccepted
-function checkIfConditionsAccepted(element) {
-  if (!element.checked) {
-    ErrorMessage(element);
-    return false; 
-    
-  }
-    removeErrorMessage();
-    return true;
-
-}
-//CheckInputValue
-function CheckInputValue(regex, element) {
-  if (!regex.test(element.value)) {
-    ErrorMessage(element);
-    return false;
-
-  }
-  removeErrorMessage();
-  return true;
-}
-
-
-
-// Show error message
-function ErrorMessage(element) {
-  const parent = element.parentElement;
-  parent.setAttribute("data-error-visible", "true");
-
-}
-
-// remove error message
-function removeErrorMessage() {
-  const errorMsg = document.querySelectorAll("[data-error]");
-  errorMsg.forEach((errorMsg) => {
-    errorMsg.removeAttribute('data-error-visible');
-  });
-}
+let formIsValid; // initialize form validation;
 
 function editNav() {
   let navbar = document.getElementById("myTopnav");
   if (navbar == null) throw new Error('navbar not found');
   navbar.classList.toggle("responsive")
-
 }
-
+// close modal form
 function closeModal() {
-  if (modalBackground != null) {
-    modalBackground.classList.remove("active");
-  }
+  modalBackground.style.display = "none";
 }
-
+// launch modal form
 function launchModal() {
-  if (modalBackground != null) {
-    modalBackground.classList.add("active");
+  modalBackground.style.display = "block";
 
+}
+function fermer(){
+  closeModal();
+  if(modalBackground.classList.contains('formSubmitted')){
+    restartModal();
   }
-
 }
 
 
+ function CloseSucess(){
+  closeModal();
+  restartModal();
+}
+// restart modal form
+function restartModal(){
+  modalBackground.classList.remove('formSubmitted');
+  modalBody.style.opacity = "1";
+  modalSuccess.style.display = "none";
+  formFirst.value="";
+  formLast.value="";
+  formEmail.value="";
+  formBirthdate.value="";
+  formQuantity.value="";
+  formLocationCheck.checked = false;
+}
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalClose.addEventListener("click",fermer);
+modalCloseSucess.addEventListener("click",CloseSucess);
+//addEventListenner
+formFirst.addEventListener("input",formFirstIsValid);
+formLast.addEventListener("input", formLastIsValid);
+formEmail.addEventListener("input",formEmailIsValid);
+formBirthdate.addEventListener("input",formBirthdateIsValid);
+formQuantity.addEventListener("input",formQuantityIsValid);
+formTermsConditions.addEventListener("change",formTermsConditionsIsValid);
 
 
+// Add error message
+function addFormErrorMessage(element, errorMessage){
+  element.parentElement.setAttribute('data-error', errorMessage);
+  element.parentElement.setAttribute('data-error-visible', 'true');
+}
 
+// Remove error message
+function removeFormErrorMessage(element){
+    element.parentElement.removeAttribute('data-error');
+    element.parentElement.removeAttribute('data-error-visible');
+}
 
+// Check the validation of the firstname input
+function formFirstIsValid(){
+  if(formFirst.value == "" || formFirst.value.length < 2 || regexName.test(formFirst.value) != true){
+    addFormErrorMessage(formFirst, "Veuillez entrer 2 caractères valide ou plus pour le champ du prénom.");
+    formIsValid = false;
+  } else{
+    removeFormErrorMessage(formFirst);
+  }
+}
 
+// // Check the validation of the lastname input
+// function formLastIsValid(){
+//   if(formLast.value == "" || formLast.value.length < 2 || regexName.test(formLast.value) != true){
+//     addFormErrorMessage(formLast, "Veuillez entrer 2 caractères valide ou plus pour le champ du nom.");
+//     formIsValid = false;
+//   } else{
+//     removeFormErrorMessage(formLast);
+//   }
+// }
 
+// // Check the validation of the email input
+// function formEmailIsValid(){
+//   if (regexEmail.test(formEmail.value) != true){
+//     addFormErrorMessage(formEmail, "Veuillez entrer une adresse email valide.");
+//     formIsValid = false;
+//   } else{
+//     removeFormErrorMessage(formEmail);
+//   }
+// }
 
+// // Check the validation of the birthdate input
+// function formBirthdateIsValid(){
+//   if(formBirthdate.value == ""){
+//     addFormErrorMessage(formBirthdate, "Vous devez entrer votre date de naissance.");
+//     formIsValid = false;
+//   } else if (Date.parse(formBirthdate.value) > Date.now()){
+//     addFormErrorMessage(formBirthdate, "Vous devez entrer une date de naissance valide.");
+//     formIsValid = false;
+//   } else {
+//     removeFormErrorMessage(formBirthdate);
+//   }
+// }
 
+// // Check the validation of the quantity input
+// function formQuantityIsValid(){
+//   if(formQuantity.value == "" || regexQuantity.test(formQuantity.value)!= true){
+//     addFormErrorMessage(formQuantity, "Veuillez entrer un nombre entre 0 et 99");
+//     formIsValid = false;
+//   } else{
+//     removeFormErrorMessage(formQuantity);
+//   }
+// }
 
+// // Check the validation of the location input
+// function formLocationIsValid(){
+//   if(formLocationCheck == null){
+//     addFormErrorMessage(formLocation, "Vous devez choisir une option.");
+//     formIsValid = false;
+//   } else{
+//     removeFormErrorMessage(formLocation);
+//   }
+// }
 
+// // Check the validation of the terms conditions input
+// function formTermsConditionsIsValid(){
+//   if(!formTermsConditions.checked){
+//     addFormErrorMessage(formTermsConditions, "Vous devez vérifier que vous acceptez les termes et conditions.");
+//     formIsValid = false;
+//   } else{
+//     removeFormErrorMessage(formTermsConditions);
+//   }
+// }
 
+// // Check the validation of the form and return a message if it's ok
+// function validate(event){
+//   //Prevent to submit form
+//   event.preventDefault();
 
+//   // Refresh form location check each time we submit
+//   formLocationCheck = document.querySelector('input[name="location"]:checked');
+//   formIsValid = true;
 
+//   formFirstIsValid();
+//   formLastIsValid();
+//   formEmailIsValid();
+//   formBirthdateIsValid();
+//   formQuantityIsValid();
+//   formLocationIsValid();
+//   formTermsConditionsIsValid();
+
+//   if(formIsValid){
+//     modalBackground.classList.add('formSubmitted');
+//     modalBody.style.opacity = "0";
+//     modalSuccess.style.display = "flex";
+//     return true;
+//   } else{
+//     return false;
+//   }
+// }
